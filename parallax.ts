6b3fc6b0620b132083cb30cab8a2d17c4ce20156
thornbeam <thornbeam@posteo.eu>
2024-04-01 22:02:02 +0200

    fix: flickering on scroll

--- parallax.ts
+++ parallax.ts
@@ -13,31 +13,33 @@ class main {
 
   fullscreen() {
     if (this.backgroundField && this.imageContainer) {
+      const imageMargin = window.innerHeight + 0.1;
+
       this.imageContainer.style.top = "unset";
-      this.imageContainer.style.bottom = -window.innerHeight * 0.1 + "px";
+      this.imageContainer.style.bottom =
+        -imageMargin - imageMargin * 0.1 + "px";
 
       window.addEventListener("scroll", () => {
         const elemTop = this.backgroundField.getBoundingClientRect().top;
         const elemBottom = this.backgroundField.getBoundingClientRect().bottom;
-        const parallaxMargin = window.innerHeight * 0.1;
 
+        // for resize, reload
         this.imageContainer.style.top = "unset";
-        this.imageContainer.style.bottom = -parallaxMargin + "px";
+        this.imageContainer.style.bottom =
+          -imageMargin - imageMargin * 0.1 + "px";
 
         if (
-          elemTop < window.innerHeight + parallaxMargin &&
-          elemBottom + window.innerHeight > window.innerHeight - parallaxMargin
+          elemTop < window.innerHeight + imageMargin &&
+          elemBottom + window.innerHeight > window.innerHeight - imageMargin
         ) {
           this.imageContainer.style.zIndex = "-10";
           this.imageContainer.style.visibility = "visible";
 
-          const visibleHeight = window.innerHeight + parallaxMargin - elemTop;
+          const visibleHeight = window.innerHeight + imageMargin - elemTop;
+          this.imageContainer.style.height =
+            "calc(" + (visibleHeight + imageMargin) + "px)";
           this.imageContainer.style.transform =
             "translateY(-" + visibleHeight * 0.1 + "px)";
-          this.imageContainer.style.height =
-            "calc(" +
-            (window.innerHeight - elemTop + parallaxMargin * 2) +
-            "px)";
         } else {
           this.imageContainer.style.zIndex = "-15";
           this.imageContainer.style.visibility = "hidden";
